1e245ea0e51b81378edef4d24060e8f1845be4cb
김수영 <swimkim@icon.foundation>
2019-02-11 18:51:52 +0900

    Everything working now

--- src/main/java/com/sooyoung/springbootadvanced/config/SecurityConfig.java
+++ src/main/java/com/sooyoung/springbootadvanced/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -15,11 +17,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	UserDetailsService customUserDetailsService;
-//	BasicAuthenticationEntryPoint authenticationEntryPoint;
+	
+	@Autowired
+	BasicAuthenticationEntryPoint authenticationEntryPoint;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(authenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 //		http.authorizeRequests().
 //		anyRequest().authenticated()
